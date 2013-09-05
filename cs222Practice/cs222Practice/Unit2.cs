e5053992b90fee48a781d2fe2dc2a5153fae44c1
Dipankar Biswas <dipankar_cse@yahoo.com>
2013-09-05 16:20:13 +0600

    some changes

--- cs222Practice/cs222Practice/Unit2.cs
+++ cs222Practice/cs222Practice/Unit2.cs
@@ -18,16 +18,23 @@ namespace cs222Practice
         double radius;
         double speed;
 
-        public Unit2()
-        {
-            Part1();
-        }
+        double h = 5.0;//s 
+        double spacecraftMass = 30000.0d;// kg 
 
-        private void Part1()
+        List<double> hArray;
+        List<double> eulerErrorArray;
+        List<double> heunsErrorArray;
+
+        public Unit2()
         {
             radius = Math.Pow((gravitationalConstant * earthMass * Math.Pow(totalTime, 2) / 4.0d / Math.Pow(Math.PI, 2)), (1.0d / 3.0d));
             speed = 2.0 * Math.PI * radius / totalTime;
 
+            Part4();
+        }
+
+        private void Part1()
+        {
             StringBuilder sb = new StringBuilder();
             sb.Append("h,e" + Environment.NewLine);
             foreach (var numSteps in new int[] { 200, 500, 1000, 2000, 5000, 10000 })
@@ -39,7 +46,44 @@ namespace cs222Practice
 
             File.WriteAllText("orbit_errror.csv", sb.ToString());
         }
+        private void Part2()
+        {
+            hArray = new List<double>();
+            eulerErrorArray = new List<double>();
+            heunsErrorArray = new List<double>();
 
+            foreach (var numSteps in new int[] { 50, 100, 200, 500, 1000 })
+            {
+                Vector2D[] x;
+                Vector2D[] v;
+                double error;
+                HeunsMethod(numSteps, out x, out v, out error);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("step size,error(euler),error(heuns)" + Environment.NewLine);
+            for (int i = 0; i < hArray.Count; i++)
+            {
+                sb.Append(hArray[i] + "," + eulerErrorArray[i] + "," + heunsErrorArray[i] + Environment.NewLine);
+            }
+            File.WriteAllText("programming_heuns.csv", sb.ToString());
+        }
+
+
+        private void Part3()
+        {
+            totalTime = 12500.0d;
+            Vector2D x, v;
+            Orbit(out x, out v);
+            Console.WriteLine("done...");
+        }
+
+        private void Part4()
+        {
+            Vector2D[] x;
+            double[] energy;
+            TotalEnergy(out x, out energy);
+        }
 
         private Vector2D Acceleration(Vector2D spaceshipPosition)
         {
@@ -70,6 +114,113 @@ namespace cs222Practice
                 v[step + 1] = v[step] + h * Acceleration(x[step]);
             }
             e = (x[numSteps] - x[0]).Length();
+        }
+
+        private void HeunsMethod(int numSteps, out Vector2D[] x, out Vector2D[] v, out double error)
+        {
+            double h = totalTime / numSteps;
+            x = new Vector2D[numSteps + 1];
+            v = new Vector2D[numSteps + 1];
+            for (int i = 0; i < numSteps + 1; i++)
+            {
+                x[i] = new Vector2D();
+                v[i] = new Vector2D();
+            }
+            x[0].x = radius;
+            v[0].y = speed;
+
+            for (int step = 0; step < numSteps; step++)
+            {
+                x[step + 1] = x[step] + h * v[step];
+                v[step + 1] = v[step] + h * Acceleration(x[step]);
+            }
+
+            error = (x[numSteps] - x[0]).Length();
+
+            hArray.Add(h);
+            eulerErrorArray.Add(error);
+
+            //Heun's Method
+            for (int step = 0; step < numSteps; step++)
+            {
+                Vector2D initialAcceleration = Acceleration(x[step]);
+                Vector2D xE = x[step] + h * v[step];
+                Vector2D vE = v[step] + h * initialAcceleration;
+
+                x[step + 1] = x[step] + h * 0.5 * (v[step] + vE);
+                v[step + 1] = v[step] + h * 0.5 * (initialAcceleration + Acceleration(xE));
+            }
+
+            error = (x[numSteps] - x[0]).Length();
+            heunsErrorArray.Add(error);
+        }
+
+        private void Orbit(out Vector2D x, out Vector2D v)
+        {
+            x = new Vector2D(); // m
+            v = new Vector2D(); // m / s
+            x.x = 15e6;
+            x.y = 1e6;
+            v.x = 2e3;
+            v.y = 4e3;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("x.x,x.y,s" + Environment.NewLine);
+            sb.Append(x.x + "," + x.y + "," + 4 + Environment.NewLine);
+
+            double currentTime = 0.0d; // s
+            double h = 100.0d;// s
+            double hNew = h;// s, will store the adaptive step size of the next step
+            double tolerance = 5e5;//m
+
+            while (currentTime < totalTime)
+            {
+                Vector2D acceleration0 = Acceleration(x);
+                Vector2D xE = x + h * v;
+                Vector2D vE = v + h * acceleration0;
+                Vector2D xH = x + h * 0.5 * (v + vE);
+                Vector2D vH = v + h * 0.5 * (acceleration0 + Acceleration(xE));
+                x = xH;
+                v = vH;
+
+                double error = (xE - xH).Length() + totalTime * ((vE - vH).Length());
+                hNew = h * Math.Sqrt(tolerance / (error + 1e-50));
+                hNew = Math.Min(1800.0d, Math.Max(0.1d, hNew));
+
+                sb.Append(x.x + "," + x.y + "," + 1 + Environment.NewLine);
+                currentTime += h;
+                h = hNew;
+            }
+
+            sb.Append(0 + "," + 0 + "," + 0 + Environment.NewLine);
+
+            File.WriteAllText("adaptive_step_size.csv", sb.ToString());
+        }
+
+        private void TotalEnergy(out Vector2D[] x, out double[] energy)
+        {
+            int numSteps = 20000;
+            x = new Vector2D[numSteps + 1]; // m
+            Vector2D[] v = new Vector2D[numSteps + 1]; // m / s
+            energy = new double[numSteps + 1];//  J = kg m2 / s2
+
+            for (int i = 0; i < numSteps + 1; i++)
+            {
+                x[i] = new Vector2D();
+                v[i] = new Vector2D();
+            }
+
+            x[0].x = 15e6;
+            x[0].y = 1e6;
+            v[0].x = 2e3;
+            v[0].y = 4e3;
+
+            for (int step = 0; step < numSteps; step++)
+            {
+                x[step + 1] = x[step] + h * v[step];
+                v[step + 1] = v[step] + h * Acceleration(x[step]);
+            }
+
         }
     }
 }
